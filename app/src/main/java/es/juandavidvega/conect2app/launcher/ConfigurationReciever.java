a356fe7b24473260b0882b3a255e9dd6e0ac7ae8
Juan David Vega <jdvr.93@hotmail.com>
2015-05-24 17:27:17 +0100

    problems with UTF-8 encoding

--- app/src/main/java/es/juandavidvega/conect2app/launcher/ConfigurationReciever.java
+++ app/src/main/java/es/juandavidvega/conect2app/launcher/ConfigurationReciever.java
@@ -1,30 +1,35 @@
 package es.juandavidvega.conect2app.launcher;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Binder;
-import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
+import org.json.JSONException;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import es.juandavidvega.conect2app.models.connect.model.Item;
+import es.juandavidvega.conect2app.models.connect.view.App;
+import es.juandavidvega.conect2app.models.connect.view.DeviceInfo;
 import es.juandavidvega.conect2app.models.connect.view.SerializableConfiguration;
 import es.juandavidvega.conect2app.remote.configure.Configurable;
-import es.juandavidvega.conect2app.remote.interoperability.HttpJson;
-import es.juandavidvega.conect2app.remote.model.AppPreview;
+import es.juandavidvega.conect2app.remote.interoperability.RequestSender;
+import es.juandavidvega.conect2app.remote.interoperability.ResponseHandler;
 
 import static es.juandavidvega.conect2app.launcher.Config.*;
 
 public class ConfigurationReciever extends Service {
 
 
+    public static final String ReadApps = "#read-apps#";
     private Configurable home;
 
 
@@ -41,11 +46,68 @@ public class ConfigurationReciever extends Service {
         if(intent.getExtras().get(MESSAGE_KEY) == null) return 1;
         String appsJson = intent.getExtras().get(MESSAGE_KEY).toString();
         Log.e("ConfigurationService", appsJson);
+        if(appsJson.contains(ReadApps)) try {
+            return readApps();
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return 1;
+        }
+        return updateApps(appsJson);
+    }
+
+    private int readApps() throws JSONException {
+        DeviceInfo deviceInfo = new DeviceInfo();
+        addCredentials(deviceInfo);
+        deviceInfo.setApps(listInstalledApps());
+        new RequestSender(getApplicationContext(), new ResponseHandler() {
+            @Override
+            public boolean onResponse(String response) {
+                Log.e("ConfigurationReceiver", response);
+                return false;
+            }
+        }).send(deviceInfo);
+        return 1;
+    }
+
+    private App[] listInstalledApps() {
+        PackageManager packageManager = getPackageManager();
+        List<ApplicationInfo> installedApplications = filterInstalledApps(packageManager.getInstalledApplications(PackageManager.GET_META_DATA));
+        List<App> apps = new ArrayList<>();
+        for (ApplicationInfo installedApplication : installedApplications) {
+            App app = new App();
+            app.setId(installedApplication.packageName);
+            app.setName(String.valueOf(installedApplication.loadLabel(getPackageManager())));
+            apps.add(app);
+        }
+        return apps.toArray(new App[apps.size()]);
+    }
+
+    private List<ApplicationInfo> filterInstalledApps(List<ApplicationInfo> installedApplications) {
+        List<ApplicationInfo> applist = new ArrayList<>();
+        for (ApplicationInfo info : installedApplications) {
+            try {
+                if (getPackageManager().getLaunchIntentForPackage(info.packageName) != null) {
+                    applist.add(info);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return applist;
+    }
+
+    private void addCredentials(DeviceInfo deviceInfo) {
+        SharedPreferences preferences = getSharedPreferences(PreferencesName, Context.MODE_PRIVATE);
+        deviceInfo.setUser(preferences.getString("user", null));
+        deviceInfo.setDevice(preferences.getString("phone", null));
+    }
+
+    private int updateApps(String appsJson) {
         SerializableConfiguration serializableConfiguration = new Gson().fromJson(appsJson, SerializableConfiguration.class);
         pushData(serializableConfiguration);
         return 1;
     }
-
 
 
     @Override
